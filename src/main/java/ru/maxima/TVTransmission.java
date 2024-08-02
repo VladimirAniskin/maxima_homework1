6e828b0f05ba8b7ca78633c3314c7b1d38b2276a
Владимир <vova1210@yandex.ru>
2024-08-02 16:20:09 +0300

    DZ8

---
+++ src/main/java/ru/maxima/TVTransmission.java
@@ -0,0 +1,13 @@
+package ru.maxima;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class TVTransmission {
+    private String programName;
+}
